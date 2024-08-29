fe98036ec32f740d0a96d37a37550d16f3793b19
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2024-08-29 13:16:27 -0400

    update changes from demo

--- SemanticKernelSample/Program.cs
+++ SemanticKernelSample/Program.cs
@@ -13,34 +13,30 @@ var kernel = Kernel.CreateBuilder()
         apiKey: apiKey!)
     .Build();
 
-// Example 1. Invoke the kernel with a prompt and display the result
-Console.WriteLine(await kernel.InvokePromptAsync("What color is the sky?"));
-Console.WriteLine();
-Console.WriteLine("============================================================");
+// Add services to the container.
+builder.Services.AddRazorPages();
 
-// Example 2. Invoke the kernel with a templated prompt and display the result
-// KernelArguments arguments = new() { { "topic", "sea" } };
-// Console.WriteLine(await kernel.InvokePromptAsync("What color is the {{$topic}}?", arguments));
-// Console.WriteLine();
-// Console.WriteLine("============================================================");
+builder.Services.AddScoped(_ => kernel);
+var chatHistory = new ChatHistory();
+builder.Services.AddScoped(_ => chatHistory);
 
-// Example 3. Invoke the kernel with a templated prompt and stream the results to the display
-// await foreach (var update in kernel.InvokePromptStreamingAsync("What color is the {{$topic}}? Provide a detailed explanation.", arguments))
-// {
-//     Console.Write(update);
-// }
+var app = builder.Build();
 
-// Console.WriteLine(string.Empty);
-// Console.WriteLine("============================================================");
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
-// Example 4. Invoke the kernel with a templated prompt and execution settings
-// arguments = new(new OpenAIPromptExecutionSettings { MaxTokens = 500, Temperature = 0.5 }) { { "topic", "dogs" } };
-// Console.WriteLine(await kernel.InvokePromptAsync("Tell me a story about {{$topic}}", arguments));
-// Console.WriteLine("============================================================");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-// Example 5. Invoke the kernel with a templated prompt and execution settings configured to return JSON
-// #pragma warning disable SKEXP0010
-// arguments = new(new OpenAIPromptExecutionSettings { ResponseFormat = "json_object" }) { { "topic", "chocolate" } };
-// Console.WriteLine(await kernel.InvokePromptAsync("Create a recipe for a {{$topic}} cake in JSON format", arguments));
-// Console.WriteLine("============================================================");
-//
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
